f49e95dec6c7d77eb598cdd8eaad00f060b2adf4
Jesse <jesse.kerridgebyrne@my.jcu.edu.au>
2018-08-14 08:14:27 +1000

    Add password check program.

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,13 @@
 """
 Jesse Kerridge-Byrne
 """
+
+try:
+    entered_password = input("Please enter a password: ")
+    LENGTH = 8
+    while len(entered_password) < LENGTH:
+        entered_password = input("Please enter a complex password: ")
+    for i in range(len(entered_password)):
+        print("*", end="")
+except:
+    pass
